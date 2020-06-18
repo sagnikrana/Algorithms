583d5a5ec8ecd74780a7506fb068acb52f18743c
sagnik-rana <sagnik.rana@atworksys.com>
2020-06-18 15:39:11 -0400

    adding the notebook for week2

--- week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -1,6 +1,7 @@
 # Uses python3
 import sys
 
+
 def gcd_naive(a, b):
     current_gcd = 1
     for d in range(2, min(a, b) + 1):
@@ -10,7 +11,42 @@ def gcd_naive(a, b):
 
     return current_gcd
 
+
+def find_greater_smaller(operation, x, y):
+    if operation == 'smaller':
+        if x < y:
+            return x
+        else:
+            return y
+    elif operation == 'bigger':
+        if x > y:
+            return x
+        else:
+            return y
+    else:
+        return 0
+
+
+def gcd_fast(a, b):
+    flag = True
+    if a % b == 0 or b % a == 0:
+        return find_greater_smaller('smaller', a, b)
+    else:
+        while (flag):
+            a = find_greater_smaller('bigger', a, b)
+            b = find_greater_smaller('smaller', a, b)
+            remainder = a % b
+            if remainder == b:
+                return a
+            elif remainder == 0:
+                return 0
+            else:
+                b = remainder
+
+
 if __name__ == "__main__":
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+    #input = sys.stdin.read()
+    input_from_user = input()
+    a, b = map(int, input_from_user.split())
+    print(gcd_fast(a, b))
+
