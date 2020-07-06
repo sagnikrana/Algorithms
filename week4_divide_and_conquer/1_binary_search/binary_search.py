ef23ad8652284d3cedafe8877a8176b70312f3f4
sagnik-rana <sagnik.rana@atworksys.com>
2020-07-06 10:20:54 -0400

    creating checkpoint

--- week4_divide_and_conquer/1_binary_search/binary_search.py
+++ week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,9 +1,35 @@
 # Uses python3
 import sys
+import math
 
-def binary_search(a, x):
-    left, right = 0, len(a)
+
+def binary_search(a, low, high, x):
     # write your code here
+    mid = low + math.floor((high - low) / 2)
+
+    if len(a) == 1:
+        if a[0] == x:
+            return 0
+        else:
+            return -1
+
+    elif a[mid] == x:
+        return mid
+
+    elif mid == low and a[mid] != x:
+        if a[high] == x:
+            return high
+        else:
+            return -1
+
+    elif a[mid] < x:
+        low = mid
+        return binary_search(a, low, high, x)
+
+    elif a[mid] > x:
+        high = mid
+        return binary_search(a, low, high, x)
+
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -11,12 +37,18 @@ def linear_search(a, x):
             return i
     return -1
 
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n = data[0]
     m = data[n + 1]
-    a = data[1 : n + 1]
+    a = data[1: n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, 0, len(a)-1, x), end=' ')
+        
+    # inputs
+    # 5 1 5 8 12 13 5 8 1 23 1 11
+    # 5 1 5 8 12 13 1 23
+    # 1 1 1 0
--- week4_divide_and_conquer/2_majority_element/majority_element.py
+++ week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,18 +1,23 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
+def get_majority_element(a, low, high):
+    if low == high or len(a) == 0:
+        return 0
+
+    main_dict = {}
+    
+    for item in a:
+        if item in main_dict:
+            main_dict[item] = main_dict[item] + 1
+            if main_dict[item] > high/2:
+                return 1
+        else:
+            main_dict[item] = 1
     #write your code here
-    return -1
+    return 0
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
-        print(1)
-    else:
-        print(0)
+    print(get_majority_element(a, 0, n))
